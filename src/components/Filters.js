fc58843cdf5c6423109de95ea391be09aa0dacdf
David Salami <wizdave97@gmail.com>
2020-05-29 07:47:58 +0100

    feat: Declared application routes

---
+++ src/components/Filters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Filters = (props) => {
+
+    return (
+        <div className='w-full p-4'>
+            <h1 className='text-gray-600 text-lg'>Filters</h1>
+        </div>
+    )
+}
+export default Filters;
---
+++ src/components/People.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const People = (props) => {
+
+    return (
+        <div className='w-full p-4'>
+            <h1 className='text-gray-600 text-lg'>People</h1>
+        </div>
+    )
+}
+export default People;
