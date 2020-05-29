a824be41f07da610887fa9f115a20437931505b8
David Salami <wizdave97@gmail.com>
2020-05-29 13:36:15 +0100

    Updates

---
+++ precache-manifest.cdbc05aec74c50704cae3ca12ea079c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5c8b872563c9224ddcac38670c02bc",
+    "url": "/car-owners-frontend/index.html"
+  },
+  {
+    "revision": "cc6163be258e1e4fde06",
+    "url": "/car-owners-frontend/static/css/main.63333740.chunk.css"
+  },
+  {
+    "revision": "2ae258561802b082b446",
+    "url": "/car-owners-frontend/static/js/2.11efd427.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/car-owners-frontend/static/js/2.11efd427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6163be258e1e4fde06",
+    "url": "/car-owners-frontend/static/js/main.9d66e9de.chunk.js"
+  },
+  {
+    "revision": "d5b0a250cbb56770fc8c",
+    "url": "/car-owners-frontend/static/js/runtime-main.62cb73ed.js"
+  },
+  {
+    "revision": "4ddb474f720156c009ae70b5ba188422",
+    "url": "/car-owners-frontend/static/media/car.4ddb474f.jpg"
+  }
+]);
